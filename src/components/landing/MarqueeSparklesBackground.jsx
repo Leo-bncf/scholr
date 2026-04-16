424c7a9abe1ff391d9e8fc268f830e501b349598
Erik Gerbst <erik.gerbst@gmail.com>
2026-04-16 21:45:25 +0000

    File changes

--- src/components/landing/MarqueeSparklesBackground.jsx
+++ src/components/landing/MarqueeSparklesBackground.jsx
@@ -16,7 +16,7 @@ export default function MarqueeSparklesBackground() {
           speed={1.2}
         />
       </div>
-      <div className="absolute inset-0 bg-gradient-to-b from-white via-white/75 via-20% to-white" />
+      <div className="absolute inset-0 bg-gradient-to-b from-white via-white/35 via-20% to-transparent" />
     </div>
   );
 }
