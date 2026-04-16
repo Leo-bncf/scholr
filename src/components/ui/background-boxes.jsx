5a027c40dcb9d13d88846b2ae217aa9f6185ac46
Erik Gerbst <erik.gerbst@gmail.com>
2026-04-16 00:33:22 +0000

    File changes

--- src/components/ui/background-boxes.jsx
+++ src/components/ui/background-boxes.jsx
@@ -29,8 +29,8 @@ export const BoxesCore = ({ className, ...rest }) => {
       style={{
         transform: `translate(-40%,-60%) skewX(-48deg) skewY(14deg) scale(0.675) rotate(0deg) translateZ(0)`,
       }}
-      animate={{ x: [0, -120] }}
-      transition={{ repeat: Infinity, repeatType: "reverse", duration: 18, ease: "linear" }}
+      animate={{ x: [0, -240] }}
+      transition={{ repeat: Infinity, duration: 14, ease: "linear" }}
       className={cn(
         "absolute left-1/4 p-4 -top-1/4 flex -translate-x-1/2 -translate-y-1/2 w-full h-full z-0",
         className
